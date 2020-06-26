21b03d88f74849c7e33b985d1ce486b62b2b27ad
Aleck Zanderbier <aleckzts@gmail.com>
2020-06-26 14:37:17 -0300

    CI configuration

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,6 +2,9 @@
 
 chown -R www-data.www-data /var/www/storage
 
+cp .env.example .env
+cp .env.testing.example .env.testing
+
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 composer install
 php artisan key:generate
