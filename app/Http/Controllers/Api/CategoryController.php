4fa184f7cc60770d4c9492902ffcf05646326932
Aleck Zander <aleckzts@gmail.com>
2020-09-22 08:45:32 -0300

    Testes de category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,41 +6,57 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class CategoryController extends BasicCrudController
 {
+    protected function model()
+    {
+        return Category::class;
+    }
+
     private $rules = [
         'name' => 'required|max:255',
+        'description' => 'nullable',
         'is_active' => 'boolean'
     ];
 
-    public function index()
+    protected function rulesStore()
     {
-        return Category::all();
+        return $this->rules;
     }
 
-    public function store(Request $request)
+    protected function rulesUpdate();
     {
-        $this->validate($request, $this->rules);
-        $category = Category::create($request->all());
-        $category->refresh();
-        return $category;
+        return $this->rules;
     }
 
-    public function show(Category $category)
-    {
-        return $category;
-    }
+    // public function index()
+    // {
+    //     return Category::all();
+    // }
 
-    public function update(Request $request, Category $category)
-    {
-        $this->validate($request, $this->rules);
-        $category->update($request->all());
-        return $category;
-    }
+    // public function store(Request $request)
+    // {
+    //     $this->validate($request, $this->rules);
+    //     $category = Category::create($request->all());
+    //     $category->refresh();
+    //     return $category;
+    // }
 
-    public function destroy(Category $category)
-    {
-        $category->delete();
-        return response()->noContent(); // 204
-    }
+    // public function show(Category $category)
+    // {
+    //     return $category;
+    // }
+
+    // public function update(Request $request, Category $category)
+    // {
+    //     $this->validate($request, $this->rules);
+    //     $category->update($request->all());
+    //     return $category;
+    // }
+
+    // public function destroy(Category $category)
+    // {
+    //     $category->delete();
+    //     return response()->noContent(); // 204
+    // }
 }
