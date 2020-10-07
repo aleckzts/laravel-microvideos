573273d3631a331fa770f5b3ef18543ea068d9ca
Aleck Zander <aleckzts@gmail.com>
2020-10-07 09:55:59 -0300

    video Uploads

--- tests/Exceptions/TestException.php
+++ tests/Exceptions/TestException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Exception;
+namespace Tests\Exceptions;
 
 class TestException extends \Exception
 {
--- tests/Feature/Http/Controllers/Api/VideoControllerTest.php
+++ tests/Feature/Http/Controllers/Api/VideoControllerTest.php
@@ -191,31 +191,37 @@ class VideoControllerTest extends TestCase
         ]);
     }
 
-    // public function testStoreWithFiles()
-    // {
-    //     \Storage::fake();
-    //     $files = $this->getFiles();
+    public function testStoreWithFiles()
+    {
+        \Storage::fake();
+        $files = $this->getFiles();
 
-    //     $category = factory(Category::class)->create();
-    //     $genre = factory(Genre::class)->create();
-    //     $genre->categories()->sync($category->id);
+        $category = factory(Category::class)->create();
+        $genre = factory(Genre::class)->create();
+        $genre->categories()->sync($category->id);
 
-    //     $response = $this->json(
-    //         'POST',
-    //         $this->routeStore(),
-    //         $this->sendData + [
-    //             'categories_id' => [$category->id],
-    //             'genres_id' => [$genre->id],
-    //         ] +
-    //             $files
-    //     );
-    //     $response->assertStatus(201);
-    //     $id = $response->json('data.id');
+        $response = $this->json(
+            'POST',
+            $this->routeStore(),
+            $this->sendData + [
+                'categories_id' => [$category->id],
+                'genres_id' => [$genre->id],
+            ]
+                + $files
+        );
+        $response->assertStatus(201);
+        $id = $response->json('id');
+        foreach ($files as $file) {
+            \Storage::assertExists("$id/{$file->hashName()}");
+        }
+    }
 
-    //     foreach ($files as $file) {
-    //         \Storage::assertExists("$id/{$file->hashName()}");
-    //     }
-    // }
+    protected function getFiles()
+    {
+        return [
+            'video_file' => UploadedFile::fake()->create('video_file.mp4')
+        ];
+    }
 
     protected function model()
     {
