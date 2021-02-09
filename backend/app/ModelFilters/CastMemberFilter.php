a22800a2b2d6e6a469ebf28daac58c107d413a3d
Aleck Zander <aleckzts@gmail.com>
2021-02-09 08:16:30 -0300

    extra filters

--- backend/app/ModelFilters/CastMemberFilter.php
+++ backend/app/ModelFilters/CastMemberFilter.php
@@ -2,6 +2,7 @@
 
 namespace App\ModelFilters;
 
+use App\Models\CastMember;
 use App\ModelFilters\DefaultModelFilter;
 
 class CastMemberFilter extends DefaultModelFilter
@@ -11,5 +12,11 @@ class CastMemberFilter extends DefaultModelFilter
     public function search($search)
     {
         $this->where('name', 'LIKE', "%$search%");
+    }
+
+    public function type($type){
+        if (in_array((int)$type, CastMember::$types)) {
+            $this->orwhere('type', (int)$type);
+        }
     }
 }
--- backend/app/ModelFilters/GenreFilter.php
+++ backend/app/ModelFilters/GenreFilter.php
@@ -2,6 +2,7 @@
 
 namespace App\ModelFilters;
 
+use Illuminate\Database\Eloquent\Builder;
 use App\ModelFilters\DefaultModelFilter;
 
 class GenreFilter extends DefaultModelFilter
@@ -11,5 +12,14 @@ class GenreFilter extends DefaultModelFilter
     public function search($search)
     {
         $this->where('name', 'LIKE', "%$search%");
+    }
+
+    public function categories($categories)
+    {
+        $idsOrNames = explode(",", $categories);
+        $this->whereHas('categories', function (Builder $query) use ($idsOrNames) {
+            $query->whereIn('id', $idsOrNames)
+                  ->orWhereIn('name', $idsOrNames);
+        });
     }
 }
